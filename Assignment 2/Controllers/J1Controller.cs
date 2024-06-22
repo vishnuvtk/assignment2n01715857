9a7ce06ccd9fe0f62dab7fab9caadaa65308d451
vishnuvtk <vishnuv.kvr@gmail.com>
2024-06-21 23:34:17 -0400

    Add project files.

---
+++ Assignment 2/Controllers/J1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        /// <summary>
+        /// Calculates the total calorie count based on the selected menu items.
+        /// </summary>
+        /// <param name="burger">Index of the burger choice</param>
+        /// <param name="drink">Index of the drink choice</param>
+        /// <param name="side">Index of the side choice</param>
+        /// <param name="dessert">Index of the dessert choice</param>
+        /// <returns>
+        /// Total calorie count of the selected items
+        /// </returns>
+        /// <example>
+        /// GET ../api/J1/Menu/4/4/4/4 -> "Your total calorie count is 0"
+        /// GET ../api/J1/Menu/1/2/3/4 -> "Your total calorie count is 691"
+        /// </example>
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string Menu(int burger, int drink, int side, int dessert) 
+        {
+            int[] burgerCalories = { 0, 461, 431, 420, 0 };
+            int[] drinkCalories = { 0, 130, 160, 118, 0 };
+            int[] sideCalories = { 0, 100, 57, 70, 0 };
+            int[] dessertCalories = { 0, 167, 266, 75, 0 };
+
+            int totalCalories = burgerCalories[burger] + drinkCalories[drink] + sideCalories[side] + dessertCalories[dessert];
+
+            return ("Your total calorie count is " + totalCalories);
+
+        }
+    }
+}
---
+++ Assignment 2/Controllers/J2Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /// <summary>
+        /// Determines how many ways two dice can roll the value of 10.
+        /// </summary>
+        /// <param name="m">Number of sides on the first die</param>
+        /// <param name="n">Number of sides on the second die</param>
+        /// <returns>Number of ways to get the sum of 10</returns>
+        /// <example>
+        /// GET ../api/J2/DiceGame/6/8 -> "There are 5 total ways to get the sum 10."
+        /// GET ../api/J2/DiceGame/12/4 -> "There are 4 ways to get the sum 10."
+        /// </example>
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string DiceGame(int m, int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (i + j == 10)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return ("There are " + count + " total ways to get the sum 10.");
+        }
+    }
+}
